3d9671c1808e1dd5a72c06b89b4d7889489adc9f
张令会 <linghui.zhang@huihui.local>
2021-12-02 20:59:21 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+function Remove(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button type="button"
+      style={{ marginBottom: '10px' }}
+      onClick={() =>
+        props.removeComplete()}>remove complete</button>
+  )
+}
+
+function TodoList(props) {
+  const list = props.todoList;
+  const listItems = list.map((item, index) =>
+    <li key={index}>
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={(e) => {
+          list[index].checked = !list[index].checked;
+          props.updateList(list)
+        }}
+      />
+      <span>{item.name}</span>
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
   );
 }
 
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoItem, setTodoItem] = useState('');
+  const [complete, setComplete] = useState(1);
+  const updateList = (data) => {
+    const tempList = JSON.parse(JSON.stringify(data))
+    setTodoList(tempList)
+  }
+  const removeComplete = () => {
+    setTodoList(todoList.filter(item => item.checked === false))
+  }
+
+  useEffect(() => {
+    setComplete(todoList.filter(item => item.checked === false).length)
+  }, [todoList]);
+
+  return (
+    <div style={{ margin: '100px' }}>
+      <div>
+        <span style={{ marginRight: '10px' }}>{complete} of {todoList.length} todo</span>
+        <Remove removeComplete={removeComplete} />
+      </div>
+
+      <TodoList todoList={todoList} updateList={updateList} />
+      <div>
+        <input
+          type="text"
+          value={todoItem}
+          onChange={(e) => { setTodoItem(e.target.value) }}
+        />
+        <input
+          type="submit"
+          value="add"
+          onClick={() => {
+            const item = { name: todoItem, checked: false };
+            const tempList = JSON.parse(JSON.stringify(todoList))
+            tempList.push(item);
+            setTodoList(tempList);
+            setTodoItem('')
+          }}
+        />
+      </div>
+
+    </div>
+  )
+}
 export default App;
